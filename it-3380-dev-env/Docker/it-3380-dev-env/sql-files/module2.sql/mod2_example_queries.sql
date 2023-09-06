39bc722b572f62f071820d9ce3d6725f145628d4
jlayton818 <90067766+jlayton818@users.noreply.github.com>
2023-09-05 21:40:31 -0500

    Delete it-3380-dev-env/Docker/it-3380-dev-env directory

--- it-3380-dev-env/Docker/it-3380-dev-env/sql-files/module2.sql/mod2_example_queries.sql
+++
@@ -1,5 +0,0 @@
-select * from customer;
-select * from agents;
-select cust_name, grade from customer;
-select agent_code, agent_name, working_area from agents;
-select ord_num, ord_amount, ord_date from orders;
